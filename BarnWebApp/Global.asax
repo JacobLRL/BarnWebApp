da9569ecb19059cba2547a44ecebf0e00a6fe1e3
JakeLRL <JakeLRL@hotmail.com>
2021-01-26 09:26:38 +0000

    first commit, login, some layout, roles

---
+++ BarnWebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BarnWebApp.MvcApplication" Language="C#" %>
---
+++ BarnWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BarnWebApp.Startup))]
+namespace BarnWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
